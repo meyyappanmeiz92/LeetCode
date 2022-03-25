99860a59997ce21eba3a62b0cc314ae6c797ef8a
Nachiappan, Meyyappan <meyyappannachiappan@rocketmortgage.com>
2022-03-25 13:05:54 -0400

    code for unsorted array

--- TwoSumLeetCode/TwoSumLeetCode/Program.cs
+++ TwoSumLeetCode/TwoSumLeetCode/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] nums = new int[4]{ 2, 7, 11, 15 };
+int target = 9;
+TwoSum(nums, target);
+
+int[] TwoSum(int[] nums, int target)
+{
+    int[] num = new int[2];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                num[0] = i;
+                num[1] = j;
+            }
+        }
+    }
+
+    return num;
+}
