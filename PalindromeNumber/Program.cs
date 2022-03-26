c1f003efec2c5ad74c1847a50854e77a77d9a02f
Nachiappan, Meyyappan <meyyappannachiappan@rocketmortgage.com>
2022-03-26 07:32:44 -0400

    first solution for integer palindrome

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int x = 34532345;
+IsPalindrome(x);
+
+bool IsPalindrome(int x)
+{
+    if (x < 0)
+    {
+        return false;
+    }
+
+    string s = x.ToString();
+    char[] reverse = s.ToCharArray();
+    Array.Reverse(reverse);
+    string temp = new string(reverse);
+    if (s == temp)
+    {
+        return true;
+    }
+
+    return false;
+}
