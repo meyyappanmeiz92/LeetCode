ae1a129c53076a2f00f5d73922e3f93b6540075f
Nachiappan, Meyyappan <meyyappannachiappan@rocketmortgage.com>
2022-03-26 08:24:38 -0400

    2nd solution for palindrome number

--- PalindromeNumber/Program.cs
+++ PalindromeNumber/Program.cs
@@ -1,24 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/// <summary>
+/// convert int to string, string to char and use Array Reverse
+/// </summary>
+//int x = 34532345;
 
-int x = 34532345;
-IsPalindrome(x);
+//IsPalindrome(x);
 
-bool IsPalindrome(int x)
+//bool IsPalindrome(int x)
+//{
+//    if (x < 0)
+//    {
+//        return false;
+//    }
+
+//    string s = x.ToString();
+//    char[] reverse = s.ToCharArray();
+//    Array.Reverse(reverse);
+//    string temp = new string(reverse);
+//    if (s == temp)
+//    {
+//        return true;
+//    }
+
+//    return false;
+//}
+
+/// <summary>
+/// Inside while loop, use modulo operator, add to a list and 
+/// divide the given number (then subsequent reminder) until it is 0
+/// then compare the resulting array with the reversed array using SequenceEqual method
+/// </summary>
+
+ToDigitArray(121);
+int[] ToDigitArray(int x)
 {
-    if (x < 0)
+    List<int> num = new List<int>();
+    while (x != 0)
     {
-        return false;
+        num.Add(x % 10);
+        x = x / 10;
     }
 
-    string s = x.ToString();
-    char[] reverse = s.ToCharArray();
-    Array.Reverse(reverse);
-    string temp = new string(reverse);
-    if (s == temp)
+    int[] givenNumber = num.ToArray();
+    num.Reverse();
+    int[] reverseNumber = num.ToArray();
+
+    if (givenNumber.SequenceEqual(reverseNumber))
     {
-        return true;
+        return givenNumber;
     }
 
-    return false;
+    return reverseNumber;
 }
