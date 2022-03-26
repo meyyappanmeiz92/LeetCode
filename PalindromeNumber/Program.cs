1b9eab7429a36c63dd408b817bf4e4289db658c8
Nachiappan, Meyyappan <meyyappannachiappan@rocketmortgage.com>
2022-03-26 11:30:02 -0400

    optimum solution for palindrome number

--- PalindromeNumber/Program.cs
+++ PalindromeNumber/Program.cs
@@ -31,24 +31,55 @@
 /// then compare the resulting array with the reversed array using SequenceEqual method
 /// </summary>
 
-ToDigitArray(121);
-int[] ToDigitArray(int x)
+//ToDigitArray(121);
+//int[] ToDigitArray(int x)
+//{
+//    List<int> num = new List<int>();
+//    while (x != 0)
+//    {
+//        num.Add(x % 10);
+//        x = x / 10;
+//    }
+
+//    int[] givenNumber = num.ToArray();
+//    num.Reverse();
+//    int[] reverseNumber = num.ToArray();
+
+//    if (givenNumber.SequenceEqual(reverseNumber))
+//    {
+//        return givenNumber;
+//    }
+
+//    return reverseNumber;
+//}
+
+/// <summary>
+///use modulo operator to get reminder and then multiply by 10 => rem = n % 10
+///and add reminder to get the reverse of a number => reverse = reverse * 10 + rem
+///last get the quotient and continue the loop until it is 0 => n = n / 10
+/// </summary>
+IsPalindromeNumber(121);
+bool IsPalindromeNumber(int x)
 {
-    List<int> num = new List<int>();
-    while (x != 0)
+    if(x < 0)
     {
-        num.Add(x % 10);
-        x = x / 10;
+        return false;
     }
 
-    int[] givenNumber = num.ToArray();
-    num.Reverse();
-    int[] reverseNumber = num.ToArray();
+    int rem, reverse = 0;
+    int n = x;
 
-    if (givenNumber.SequenceEqual(reverseNumber))
+    while(n != 0)
     {
-        return givenNumber;
+        rem = n % 10;
+        reverse = reverse * 10 + rem;
+        n = n / 10;
     }
 
-    return reverseNumber;
+    if(x == reverse)
+    {
+        return true;
+    }
+
+    return false;
 }
