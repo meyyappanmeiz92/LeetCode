8bac7a1d2cf14fd372d51facbaba1b2521be8db9
Nachiappan, Meyyappan <meyyappannachiappan@rocketmortgage.com>
2022-03-29 21:52:39 -0400

    ValidParanthesis and RemoveDuplicatesFromSortedArray

---
+++ LeetCode/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// if given array length is less than or equal to 1, return array length
+/// if base case is not true, traverse the array from 1st element to last and
+/// initialise another pointer from 0 (lets say index)
+/// if ith pointer not equal to index pointer, assign nums[i] to nums[++index]
+/// once all the array traversal is done, return index + 1
+/// </summary>
+
+int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+RemoveDuplicates(nums);
+int RemoveDuplicates(int[] nums)
+{
+    int length = nums.Length;
+    if(length <= 1)
+    {
+        return length;
+    }
+
+    int index = 0;
+    for(int i = 1; i < length; i++)
+    {
+        if(nums[i] != nums[index])
+        {
+            nums[++index] = nums[i];
+        }
+    }
+
+    return index + 1;
+}
---
+++ LeetCode/ValidParanthesis/Program.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// Initialize Stack and do a for loop on the characters in the string.
+/// After that do a switch case on the open valid paranthesis.
+/// and do a push of corresponding close brackets.
+/// if none of the case match then in default check for stack empty or 
+/// if popped element is not the current element in the loop
+/// if one of the condition matches then return false
+/// and after exiting the loop check for stack empty and return
+/// </summary>
+
+string s = "{]";
+isValid(s);
+bool isValid(string s)
+{
+    Stack<char> stack = new Stack<char>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        char c = s[i];
+        switch (c)
+        {
+            case '[':
+                stack.Push(']');
+                break;
+            case '(':
+                stack.Push(')');
+                break;
+            case '{':
+                stack.Push('}');
+                break;
+            default:
+                if (stack.Count == 0 || stack.Pop() != c)
+                    return false;
+                break;
+        }
+    }
+    return stack.Count == 0;
+}
