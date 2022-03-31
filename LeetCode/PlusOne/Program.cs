3125e1be80e2d3baf89323ce6565a9ce1169d400
Nachiappan, Meyyappan <meyyappannachiappan@rocketmortgage.com>
2022-03-31 06:15:15 -0400

    RemoveElement and PlusOne project

---
+++ LeetCode/PlusOne/Program.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// Get the length of the array and point to the last element and start navigating using while loop
+/// if the element is 9, replace that with 0 else
+/// increase by 1 and return it.
+/// if it exits the while loop, then initialize an array with 1 length higher and
+/// make the first element 1 and return it.(since it will be all 0 by default)
+/// </summary>
+
+int[] digits = { 7, 9, 8, 9 };
+PlusOne(digits);
+int[] PlusOne(int[] digits)
+{
+    int length = digits.Length;
+    int i = length - 1;
+    while(i >= 0)
+    {
+        if(digits[i] == 9)
+        {
+            digits[i] = 0;
+        }
+        else
+        {
+            digits[i] = digits[i] + 1;
+            return digits;
+        }
+        i--;
+    }
+
+    int[] result = new int[length + 1];
+    result[0] = 1;
+
+    return result;
+}
---
+++ LeetCode/RemoveElement/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Two pointer solution
+/// Assign index to 0 and loop through each element
+/// if target != array[i], assign that value to array[index] and then increment the index
+/// After looping through all the elements, return the index value
+/// </summary>
+
+int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+RemoveElement(nums, 2);
+
+int RemoveElement(int[] nums, int value)
+{
+    int index = 0;
+    for(int i = 0; i < nums.Length; i++)
+    {
+        if(nums[i] != value)
+        {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+
+    return index;
+}
